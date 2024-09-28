083acc5ebc16179b7552a8fae58345f5ae3510f1
ykylejan <jkDellatan@mcm.edu.ph>
2024-09-28 23:56:04 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Todo from "./component/Todo";
+
+const App = () => {
+  return (
+    <div className="bg-stone-500 h-screen grid">
+      <Todo/>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/component/TodoItems.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import tick from '../assets/tick.png';
+import nottick from '../assets/not_tick.png';
+import delete_icon from '../assets/delete.png';
+
+const TodoItems = ({ text, id, isComplete, deleteTodo, toggleTodo }) => {
+  return (
+    <div className='flex items-center my-3 gap-2'>
+        
+        <div onClick={()=>{toggleTodo(id)}} className='flex flex-1 items-center cursor-pointer '>
+            <img src={isComplete? tick : nottick} alt="" className='w-7'/>
+            <p className={'text-slate-700 ml-4 text-[17px] decoration-slate-500'}> {text}</p>
+        </div>
+
+        <img onClick={()=>{deleteTodo(id)}} src={delete_icon} alt="" className='w-3.5 cursor-pointer'/>
+
+    </div>
+  )
+}
+
+export default TodoItems
